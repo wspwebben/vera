0d29fa10c13715031cef8b24f9b3a9a1e2dd094b
anpermyakov <anpermyakov@ozon.ru>
2022-08-24 22:42:38 +0300

    Update cryptocurrencies design

--- script/cryptocurrencies.js
+++ script/cryptocurrencies.js
@@ -1,20 +1,33 @@
-import Glide, { Autoplay, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm'
+import Glide, { Autoplay, Swipe, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm'
 
 export function initCryptocurrenciesSlider() {
+    const biggerThanTablet = window.matchMedia('(min-width: 768px')
+
+    if (biggerThanTablet.matches) {
+        const root = document.querySelector('.cryptocurrencies')
+        root.classList.add('cryptocurrencies--no-slider')
+
+        return
+    }
+
     new Glide('.glide', {
         type: 'carousel',
         perView: 5,
         gap: 30,
-        autoplay: 5000,
+        autoplay: 3000,
         animationTimingFunc: 'linear',
+        dragThreshold: 30,
         breakpoints: {
-            400: {
-                perView: 3,
+            440: {
+                perView: 2,
                 gap: 20,
             },
             600: {
+                perView: 3,
+            },
+            768: {
                 perView: 4,
             },
         }
-    }).mount({ Autoplay, Breakpoints })
+    }).mount({ Autoplay, Breakpoints, Swipe })
 }
