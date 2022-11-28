ca99f6d96f277b4b0dd8ea055e3f1f7f981f21dc
anpermyakov <anpermyakov@ozon.ru>
2022-11-28 23:13:09 +0400

    Add script

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/styles.css')
+    eleventyConfig.addPassthroughCopy('./src/script.js')
     eleventyConfig.addPassthroughCopy('./src/images')
     eleventyConfig.addPassthroughCopy('./src/fonts')
     eleventyConfig.addPassthroughCopy('./src/admin')
